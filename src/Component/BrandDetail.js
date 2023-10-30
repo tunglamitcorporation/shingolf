0ceacd8a28872e29f43fff95921de0f698e251c4
foundertl <nguyenductunglam0503@gmail.com>
2023-10-30 08:53:02 +0700

    branddetail

--- src/Component/BrandDetail.js
+++ src/Component/BrandDetail.js
@@ -36,77 +36,96 @@ export default function BrandDetail(){
             </div>
             <div class="branch__container">
                 <div class="container">
+                    {/* <div class="col-md-10"> */}
+                <div class="feature__type-item">
+                    <div class="card">
                     <div class="row">
-                        <div class="col-md-12">
-                            <div class="feature__type-item" style={{height:'400px'}}>
-                                <div class="col-md-5">
-                                <div class="feature__type-img" >
+                        <div class="col-md-5">
+                            <img class = "img-fluid" src ="https://res.cloudinary.com/dtdfsaaei/image/upload/v1698027483/AzumayaWeb/o8dg6xz425tf0blhj1hz.jpg" alt="" />
+                        </div>
+                        <div class="col-md-7">
+                            <div class="card__body">
+                                <div class="card_title">
+                                <h2>Hai Ba Trung 1</h2>
                                 </div>
+                                <div class="card_text">
+                                <p>Azumaya Hotel Hai Ba Trung 1 is a 31 room hotel located in the middle of Hai Ba Trung district with shower toilet, bathtub, and Japanese TV shows supplied for each room. We have a giant open-air hot bath, Japanese breakfast buffet, Japanese restaurant “Mangetsu” (6pm-2am)original style foot massage which 90% of the customers are repeating. (The last order for massage is 9:45pm.) of all branches, only Hai Ba Trung 1 branch uses the natural stone for open-air hot baths. After relaxing in the open-air hot bath, have our soothing massage, and come satisfy your appetite in “Mangetsu”; we promise you will surely forget today’s fatigue and have a nice deep sleep for next day’s work.</p>
+                                </div>
+                                </div>
+                                <div class="btn__detail">
+                                    <a href="/AzumayaClone/html/hanoibranchroom.html">Click here for detail</a>
+                                </div>
+                        </div>
+                    </div>
+                    </div>
+                    </div>
+                <div class="feature__type-item">
+                    <div class="card">
+                    <div class="row">
+                                <div class="col-md-5">
+                                <img class = "img-fluid" src ="https://res.cloudinary.com/dtdfsaaei/image/upload/v1698027481/AzumayaWeb/fersjcyqml9gtjpit7pk.jpg" alt="" />
                                 </div>
                                 <div class="col-md-7">
-                                <div class="feature__type-content">
-                                    <h2>Hai Ba Trung 1</h2>
-                                    <p>Azumaya Hotel Hai Ba Trung 1 is a 31 room hotel located in the middle of Hai Ba Trung district with shower toilet, bathtub, and Japanese TV shows supplied for each room. We have a giant open-air hot bath, Japanese breakfast buffet, Japanese restaurant “Mangetsu” (6pm-2am)original style foot massage which 90% of the customers are repeating. (The last order for massage is 9:45pm.) of all branches, only Hai Ba Trung 1 branch uses the natural stone for open-air hot baths. After relaxing in the open-air hot bath, have our soothing massage, and come satisfy your appetite in “Mangetsu”; we promise you will surely forget today’s fatigue and have a nice deep sleep for next day’s work.</p>
+                                <div class="card__body">
+                                    <div class="card_title">
+                                    <h2>Kim Ma 2</h2>
+                                    </div>
+                                    <div class="card_text">
+                                    <p>Azumaya Hotel Kim Ma 2 branch, standing at the location 3 minutes far by walk from Kim Ma Street, has 38 rooms supplied with shower toilet, bathtub, and Japanese TV shows. In addition, we provide 2 annex rooms with separate entry from the main front entrance. For the long stay customers, 11 rooms with a kitchen are available. Both hotel guests and visitors can enjoy our open-air hot bath, Japanese breakfast , original style foot massage which 90% of users are repeat customers, and Japanese restaurant “Suzunoya”. (5pm-1am) After relaxing at our open-air hot bath, soothe your fatigue at foot massage, and satisfy your appetite at “Suzunoya” ; we promise you will surely forget today’s fatigue and have a nice deep sleep for next day’s work.</p>
+                                    </div>
                                 </div>
                                 <div class="btn__detail">
                                     <a href="/AzumayaClone/html/hanoibranchroom.html">Click here for detail</a>
                                 </div>
                                 </div>
+                                </div>
+                                </div>
+                    </div>
+                <div class="feature__type-item">
+                                <div class="card">
+                                <div class="row">
+                                <div class="col-12 col-md-5">
+                                <img class = "img-fluid" src ="https://res.cloudinary.com/dtdfsaaei/image/upload/v1698027488/AzumayaWeb/pdgz4cpsqdu7lmxznh1x.jpg" alt="" />
+                                </div>
+                                <div class="col-12 col-md-7">
+                                <div class="card__body">
+                                    <div class="card_title">
+                                    <h2>Kim Ma 3</h2>
+                                    </div>
+                                    <div class="card_text">
+                                    <p>Azumaya Hotel Kim Ma 3 branch has 42 rooms in total with shower toilet and Japanese TV shows for each room. Moreover, there are open-air hot bath and Japanese breakfast buffet. The Kin Ma 1  branch strands 10 seconds away by feet and Japanese restaurant street at Kim Ma Street is also near enough to walk (about 4 minutes). Its location is much convenient at dinner time. Moreover, far from the main street and nearby the silent pond in front, you can find it peaceful to be here.</p>
+                                    </div>
+                                </div>
+                                <div class="btn__detail">
+                                    <a href="/AzumayaClone/html/hanoibranchroom.html">Click here for detail</a>
+                                </div>
+                                </div>
+                                </div>
+                                </div>
+                    </div>
+                <div class="feature__type-item">
+                    <div class="card">
+                        <div class="row">
+                            <div class="col-md-5">
+                            <img class = "img-fluid" src ="https://res.cloudinary.com/dtdfsaaei/image/upload/v1698027481/AzumayaWeb/lqhzc2ytlokliy0rtgta.png" alt="" />
                             </div>
+                            <div class="col-md-7">
+                                <div class="card__body">
+                                    <div class="card_title">
+                                    <h2>Linh  Lang</h2>
+                                    </div>
+                                    <div class="card_text">
+                                    <p>Ling Lang branch has 46 rooms in total with open-air bath, Japanese breakfast , Japanese izakaya "ISSUN Boushi" . All the rooms are equipped with shower toilet, bathtub, and Japanese TV show. The hotel is within walking distance from Lotte Centre which is located along Linh Lang road where many Japanese restaurants have opened recently. At the hotel, high-tea massage is also available on the 7th floor. It records over 90% of repeat customer rate. Last order is 9:45 pm.. You can enjoy a night view from each massage chair. It features a spacious outdoor bath. You can have a pleasant morning at the breakfast venue on the 8th floor with a fine view.</p>
+                                    </div>
+                                </div>
+                                <div class="btn__detail">
+                                    <a href="/AzumayaClone/html/hanoibranchroom.html">Click here for detail</a>
+                                </div>
+                                </div>
+                                </div>
+                                </div>
                     </div>
-                <div class="col-md-12">
-                    <div class="feature__type-item" style={{height:'400px'}}>
-                        <div class="col-md-5">
-                        <div class="feature__type-img" >
-                        </div>
-                        </div>
-                        <div class="col-md-7">
-                        <div class="feature__type-content">
-                            <h2>Kim Ma 2</h2>
-                            <p>Azumaya Hotel Kim Ma 2 branch, standing at the location 3 minutes far by walk from Kim Ma Street, has 38 rooms supplied with shower toilet, bathtub, and Japanese TV shows. In addition, we provide 2 annex rooms with separate entry from the main front entrance. For the long stay customers, 11 rooms with a kitchen are available. Both hotel guests and visitors can enjoy our open-air hot bath, Japanese breakfast , original style foot massage which 90% of users are repeat customers, and Japanese restaurant “Suzunoya”. (5pm-1am) After relaxing at our open-air hot bath, soothe your fatigue at foot massage, and satisfy your appetite at “Suzunoya” ; we promise you will surely forget today’s fatigue and have a nice deep sleep for next day’s work.</p>
-                        </div>
-                        <div class="btn__detail" >
-                            <a href="#">Click here for detail</a>
-                        </div>
-                        </div>
-                    </div>
-            </div>
-            <div class="col-md-12">
-                <div class="feature__type-item" style={{height:'400px'}}>
-                    <div class="col-md-5">
-                    <div class="feature__type-img" >
-                    </div>
-                    </div>
-                    <div class="col-md-7">
-                    <div class="feature__type-content">
-                        <h2>Kim Ma 3</h2>
-                        <p>Azumaya Hotel Kim Ma 3 branch has 42 rooms in total with shower toilet and Japanese TV shows for each room. Moreover, there are open-air hot bath and Japanese breakfast buffet. The Kin Ma 1  branch strands 10 seconds away by feet and Japanese restaurant street at Kim Ma Street is also near enough to walk (about 4 minutes). Its location is much convenient at dinner time. Moreover, far from the main street and nearby the silent pond in front, you can find it peaceful to be here.</p>
-                    </div>
-                    <div class="btn__detail"style={{marginTop:'70px'}}>
-                        <a href="#">Click here for detail</a>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-12">
-                <div class="feature__type-item" style={{height:'400px'}}>
-                    <div class="col-md-5">
-                    <div class="feature__type-img" >
-                    </div>
-                    </div>
-                    <div class="col-md-7">
-                    <div class="feature__type-content">
-                        <h2>Linh Lang</h2>
-                        <p>Ling Lang branch has 46 rooms in total with open-air bath, Japanese breakfast , Japanese izakaya "ISSUN Boushi" . All the rooms are equipped with shower toilet, bathtub, and Japanese TV show. The hotel is within walking distance from Lotte Centre which is located along Linh Lang road where many Japanese restaurants have opened recently. At the hotel, high-tea massage is also available on the 7th floor. It records over 90% of repeat customer rate. Last order is 9:45 pm.. You can enjoy a night view from each massage chair. It features a spacious outdoor bath. You can have a pleasant morning at the breakfast venue on the 8th floor with a fine view.</p>
-                    </div>
-                    <div class="btn__detail"style={{marginTop:'33px'}}>
-                        <a href="#">Click here for detail</a>
-                    </div>
-                    </div>
-                </div>
-        </div>
-                </div>
-            </div>
+                    {/* </div> */}
             </div>
             <div class="content__news" style={{marginLeft:'15%',height:'250px'}}>
                 <h2 class ="content__news-title">What's new ?</h2>
@@ -142,5 +161,6 @@ export default function BrandDetail(){
                  </ul> 
             </div>
         </div>
+        </div> 
     )
 }
