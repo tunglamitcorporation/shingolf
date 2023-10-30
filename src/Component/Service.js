d93329bb8f7f1cb590c94c0ee1518504392e1bad
foundertl <nguyenductunglam0503@gmail.com>
2023-10-30 15:30:17 +0700

    service

--- src/Component/Service.js
+++ src/Component/Service.js
@@ -1,7 +1,13 @@
+import { useState } from "react";
 import Booking from "./Booking";
-
+import {Tab, Tabs, TabList, TabPanel} from 'react-tabs'
 export default function Service() {
+    const [isActive, setActive] = useState(false)
+    const toggleClass = () =>{
+        setActive(!isActive);
+    }
     return (
+        
         <div>
             <div class="service__header">
                 <div class="container">
@@ -37,6 +43,139 @@ export default function Service() {
                 </div>
             </div>
             <div class="service__container">
+            <Tabs className="container"> 
+                <TabList className="row"style={{justifyContent:'space-around'}}>
+                    <Tab className="service__location service__active col-md-2">Ho Chi Minh</Tab> 
+                    <Tab className="service__location col-md-2">Ha Noi</Tab> 
+                    <Tab className="service__location col-md-2">Hai Phong</Tab> 
+                    <Tab className="service__location col-md-2">Da Nang</Tab> 
+                </TabList> 
+                <TabPanel> 
+                    <Tabs  defaultIndex={1} className="col-md-10 offset-md-1"> 
+                        <TabList className="service__list "> 
+                            <Tab className="service service__active">Rotenburo</Tab> 
+                            <Tab className="service">Breakfast</Tab> 
+                            <Tab className="service">Massage</Tab> 
+                        </TabList> 
+                        <TabPanel> 
+                        <div class="service__content">
+                                <div class="service__content-title col-md-12">Open-air Bathtub (Rotenburo) are open daily as Public timing For men only (Visitor are also welcome)</div>
+                                <table class="service__table">
+                                    <tr>
+                                        <th>Thai Van Lung 1</th>
+                                        <th>Thai Van Lung 2</th>
+                                        <th>Le Thanh Ton</th>
+                                    </tr>
+                                    <tr>
+                                        <td>From Mon - Fri : 6:00 to 10:00 - 17:00 to 24:00
+                                            Weekend & Holiday: 7:00 to 24:00
+                                            Price (from Feb 2023)
+                                            Azumaya Thai Văn Lung 1: 250.000vnd/1pax</td>
+                                        <td> From Mon - Fri : 6:00 to 10:00 - 19:00 to 24:00
+                                            Weekend & Holiday: 7:00 to 24:00
+                                            Price (from Jan 2019)
+                                            Azumaya Thái Văn Lung 2: 200.000/pax</td>
+                                        <td>From Mon - Fri : 6:00 to 10:00 - 17:00 to 24:00
+                                            Weekend & Holiday: 7:00 to 24:00
+                                            Price (from Jan 2019)
+                                            Azumaya Lê Thánh Tôn : 150.000/pax</td>
+                                    </tr>
+                                </table>
+                                <div class="service__content-note">※Female can use Rotenburo at Azumaya Thai Van Lung 2, From Mon - Fri : 17:00 - 18:45, price is 150.000/pax
+                                    <br />※Private - Rotenburo is available from Mon To Fri: 11:00 to 17:00 (last order 16:00)
+                                    <br />Price: 800.000vnd/hour for 1~4pax & 1.000.000vnd/hour for 5~8pax
+                                    <br />Female customer also can use. Booking a day before is required.</div>
+                            </div>
+                        </TabPanel> 
+                        <TabPanel> 
+                        <div class="service__content">
+                                <div class="service__content-title">
+                                    Azumaya Breakfast are open daily from 6am to 10am.
+                                    Visitor are also welcoming. Price for visitor is as following</div>
+                                <table class="service__table">
+                                    <tr>
+                                        <th>Thai Van Lung 1</th>
+                                        <th>Thai Van Lung 2</th>
+                                        <th>Le Thanh Ton</th>
+                                    </tr>
+                                    <tr>
+                                        <td>Buffet: 200.000VND
+                                            Set menu: from 100.000VND/set
+                                            
+                                            Serving time:
+                                            From Mon to Sat:
+                                            Buffet (from 6:00 to 8:00)
+                                            Set menu (from 8:00 to 10:00)
+                                            Sunday: only set menu (from 6:00 to 10:00)</td>
+                                        <td>Buffet: 200.000VND
+                                            Set menu: from 100.000VND/set
+                                            
+                                            Serving time:
+                                            From Mon to Sat:
+                                            Buffet (from 6:00 to 8:00)
+                                            Set menu (from 8:00 to 10:00)
+                                            Sunday: only set menu (from 6:00 to 10:00)</td>
+                                        <td>Buffet 200.000 VND
+                                            Set menu: from 100.000/set
+                                            
+                                            Serving time:
+                                            From Mon to Sat:
+                                            Buffet (from 6:00 to 8:00)
+                                            Set menu (from 8:00 to 10:00)
+                                            Sunday: only set menu (from 6:00 to 10:00)</td>
+                                    </tr>
+                                </table>
+                            </div>
+  
+                        </TabPanel> 
+                        <TabPanel> 
+                        <div class="service__content">
+                                <div class="service__content-title">
+                                    Foot tis open daily at Azumaya Thai Van Lung 1, 1st floor, are also available for visitor.</div>
+                                    <div class="service__content-body">Opening hours 10:00 - 23:00 (last entry 22: 45)
+
+                                        The course and fee are as follows.
+                                        ・40 minutes course 250,000 VND
+                                        ・70 minutes course 300,000 VND (Hotel guest: 270,000 VND)
+                                        ・100 minutes course 400,000 VND (Hotel guest: 360,000 VND)
+                                        
+                                        Rotenburo Set for Visitor
+                                        ・Set Rotenburo + Massage 40' ：400,000 VND
+                                        ・Set Rotenburo + Massage 70' : 450,000 VND
+                                        ・Set Rotenburo + Massage 100' : 550,000 VND
+                                        
+                                        * 10% discount for Hotel guests
+                                        * Tip is excluded, your satisfaction is our pleasure.
+                                        * Advance reservation is highly recommended.
+                                        Please call for booking Azumaya Thai Van Lung 1 (028) 3824 6835</div>
+                                </div>
+  
+                        </TabPanel> 
+                    </Tabs> 
+                </TabPanel> 
+                <TabPanel> 
+                    <Tabs> 
+                        <TabList> 
+                            <Tab>Rotenburo</Tab> 
+                            <Tab>Breakfast</Tab> 
+                            <Tab>Massage</Tab> 
+                        </TabList> 
+                        <TabPanel> 
+                            <p> 
+                                Welcome to GeeksforGeeks</p> 
+                        </TabPanel> 
+                        <TabPanel> 
+                            <p> 
+                                A computer science portal for geeks.</p> 
+                        </TabPanel> 
+                        <TabPanel> 
+                            <p> 
+                                Also known as GFG</p> 
+                        </TabPanel> 
+                    </Tabs> 
+                </TabPanel> 
+            </Tabs> 
+            {/* <div class="service__container">
                 <div class="container">
                     <div class="row">
                         <div class="col-md-3">
@@ -301,8 +440,8 @@ export default function Service() {
                                         Please call for booking: Azumaya Da Nang: (0236) 3743 888</div>
                                 </div>
                         </div>
-                    </div>
-                </div>
+                     </div> 
+                </div>*/}
             </div>
         </div>
     )
