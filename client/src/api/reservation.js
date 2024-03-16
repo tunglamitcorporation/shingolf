986a1d6673835475b5f24e493ab2e1e08da4dd1c
vanhaicddt2 <vanhaicddt2@gmail.com>
2024-03-16 09:14:08 +0700

    update server

--- client/src/api/reservation.js
+++ client/src/api/reservation.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export function sendReservationRequest(data, token) {
-    let newURL = "/user/send_reservation_request";
+    let newURL = "/reservation/api/4b2eae43-6f7e-459a-81fe-a68eb15522ca";
     return axios.post(`${newURL}`, data, {
         headers: { Authorization: token }
     });
