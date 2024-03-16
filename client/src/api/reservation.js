dd47940f49025dd81b079ddf9e94ca8448e2100d
vanhaicddt2 <vanhaicddt2@gmail.com>
2024-03-16 08:54:46 +0700

    update api az web

--- client/src/api/reservation.js
+++ client/src/api/reservation.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const sendReservationRequest  = (data, token) => {
+export function sendReservationRequest(data, token) {
     let newURL = "/user/send_reservation_request";
     return axios.post(`${newURL}`, data, {
         headers: { Authorization: token }
